8d8317d8b13daa6a8f971a7ddc05e39717b698ec
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-07 02:19:28 +0600

    some change and pagination

--- src/ManageItem/ManageItem.js
+++ src/ManageItem/ManageItem.js
@@ -3,16 +3,20 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import './ManageItem.css'
 
 const ManageItem = () => {
 
     const [products, setProducts] = useState([]);
+    const [limit, setLimit] = useState(5)
+    const [pageNumber, setPageNumber] = useState(0)
 
     useEffect(() => {
-        fetch('https://guarded-plains-52968.herokuapp.com/product')
+        fetch(`https://guarded-plains-52968.herokuapp.com/product?limit=${limit}&pageNumber=${pageNumber}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+
+    }, [limit, pageNumber])
 
     const handleDelete = (id) => {
         const proceed = window.confirm('Are you sure for delete?');
@@ -47,7 +51,7 @@ const ManageItem = () => {
                 </thead>
                 <tbody>
                     {
-                        products.map(product => {
+                        products?.length ? products.map(product => {
                             return <tr>
                                 <th>{product.name}</th>
                                 <td style={{ width: "100px" }} ><img className='w-100' src={product.image} alt="" /></td>
@@ -65,11 +69,23 @@ const ManageItem = () => {
                                     </Link>
                                 </td>
                             </tr>
-                        })
+                        }) : <div> No data Found</div>
                     }
 
                 </tbody>
             </table>
+            <div className='d-flex my-3 justify-content-end'>
+                {
+                    [...Array(5).keys()].map(number => <div onClick={() => setPageNumber(number)} className={`mx-3 border px-3 page ${pageNumber === number ? "bg-warning" : ""}`}>{number + 1}</div>)
+                }
+                <select onChange={(event) => setLimit(event.target.value)}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                </select>
+            </div>
             <ToastContainer></ToastContainer>
         </div>
     );
